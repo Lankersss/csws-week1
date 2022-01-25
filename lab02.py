b94b76af4b2af9a7f95a98e9a3f5864700e4783c
lydiaankers8@gmail.com <lydiaankers8@gmail.com>
2022-01-25 14:10:14 +0000

    python tests

---
+++ lab02.py
@@ -0,0 +1,15 @@
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+
+motorcycles[0] = "ducati"
+print(motorcycles)
+
+motorcycles.append("honda")
+motorcycles.append("suzuki")
+motorcycles.append("yamaha")
+print(motorcycles)
+
+vehicles = ["honda","suzuki","yamaha"]
+poppedVehicle = vehicles.pop(2)
+print(vehicles)
+print(poppedVehicle)
